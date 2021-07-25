e71b533006a189a517872e0b5129ee6199b78fe8
fmoreirag15 <francisco.moreira2017@uteq.edu.ec>
2021-07-25 04:03:59 -0500

    vinculo con los parametros

--- app/src/main/java/com/example/loginfirebasemail77/registrarpaciente.java
+++ app/src/main/java/com/example/loginfirebasemail77/registrarpaciente.java
@@ -3,12 +3,30 @@ package com.example.loginfirebasemail77;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class registrarpaciente extends AppCompatActivity {
+
+    EditText  nameTutor, firstname, lastname, birthname, gender, imagBase64, decivename,macadress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarpaciente);
+        nameTutor=findViewById(R.id.txtNameTutor);
+        firstname=findViewById(R.id.firtname);
+        lastname=findViewById(R.id.txtlastname);
+        birthname=findViewById(R.id.editTextDate);
+        gender=findViewById(R.id.firtname);
+        imagBase64=findViewById(R.id.img);
+        decivename=findViewById(R.id.txtDecivename);
+        macadress=findViewById(R.id.txtMac);
+        
+    }
+
+    public void addFirebasePaciente(View view)
+    {
+
     }
 }
